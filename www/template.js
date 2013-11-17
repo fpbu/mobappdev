4c5af5994f785dc271e0d5d0fec54d1334503668
Robert Canning <robert@mac22043.config>
2013-11-17 20:27:00 +0000

    added slides and new www

---
+++ www/template.js
@@ -0,0 +1,15 @@
+//////////////////////////////////
+// Set the event listener to run when the device is ready
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+// do stuff here
+}  
+
+/// define functions below
+
+// accelerometer
+
+// geolocation
+
+// audiorecorder
