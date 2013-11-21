b16a9859451c9cd8abbf411b195da66ad9e680dc
Robert Canning <robert@mac22043.config>
2013-11-21 10:57:32 +0000

    added script for elliott

---
+++ robsmediatoolinstaller.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+brew install \
+apple-gcc42 \
+ffmpeg --devel --with-libvpx --with-libvorbis --with-ffplay --with-opus --with-tools --with-libvo-aacenc --with-libcaca \
+ImageMagick \
+sox \
+wget curl \
+icecast \
+mplayer \
+gstreamer \
+gst-plugins-good gst-plugins-bad gst-plugins-ugly \
+opus-tools \
+inkscape \
+qrencode \
+gnupg \
+cowsay \
+tpp \
+emacs \
+ecasound \
+mlt avidemux \
+easy-tag
+
+echo "All Done!..."
